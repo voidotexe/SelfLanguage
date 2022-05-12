f946b6dc645af4ae0f54f70856fa10faa603eb60
voidotexe <91555542+voidotexe@users.noreply.github.com>
2022-05-12 13:42:41 -0300

    Refactore favpriteVideo.js

--- SelfLanguage/wwwroot/js/favoriteVideo.js
+++ SelfLanguage/wwwroot/js/favoriteVideo.js
@@ -1,19 +1,17 @@
-﻿/*
+﻿'use strict';
+
+/*
  * By: voidotexe
  * https://www.github.com/voidotexe
 */
 
-var star = document.getElementById("star");
+let star = document.getElementById("star");
 
-function handleFavoriteVideo(favoriteVideoJson) {
+star.addEventListener("click", () => {
   $.ajax({
     url: "https://localhost:5001/favoritevideo/post",
     type: "POST",
     contentType: "application/json",
     data: JSON.stringify(favoriteVideoJson)
   });
-}
-
-star.addEventListener("click", function() {
-  handleFavoriteVideo(favoriteVideoJson);
 });
